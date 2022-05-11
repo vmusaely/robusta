ad06c5034ac1135d787c8fa45f4d27d3a37765b2
vmusaely <77839501+vmusaely@users.noreply.github.com>
2022-05-11 17:34:08 +0400

    Update pod_delete.py

--- robusta_chaos/pod_delete.py
+++ robusta_chaos/pod_delete.py
@@ -2,9 +2,7 @@ from robusta.api import *
 
 
 @action
-def delete_pod(event: PodEvent):
-if not event.get_pod():
-    logging.info("alert is not related to pod")
-    return
-
-event.get_pod().delete()
+def my_action(alert: PrometheusKubernetesAlert):
+    print(f"The alert {alert.alert_name} fired on pod {alert.pod.metadata.name}")
+    print(f"The pod has these processes:", alert.pod.exec("ps aux"))
+    print(f"The pod has {len(alert.pod.spec.containers)} containers")
