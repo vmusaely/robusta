859ba826bf6d77c26a837ccf404cdc469f8b069c
Your Name <you@example.com>
2022-05-12 15:52:41 +0400

    new

--- robusta_chaos/pod_delete.py
+++ robusta_chaos/pod_delete.py
@@ -1,8 +1,9 @@
 from robusta.api import *
 
-
 @action
-def my_action(alert: PrometheusKubernetesAlert):
-    print(f"The alert {alert.alert_name} fired on pod {alert.pod.metadata.name}")
-    print(f"The pod has these processes:", alert.pod.exec("ps aux"))
-    print(f"The pod has {len(alert.pod.spec.containers)} containers")
+def delete_pod(event: PrometheusKubernetesAlert):
+if not event.get_pod():
+    logging.info("alert is not related to pod")
+    return
+
+event.get_pod().delete()
