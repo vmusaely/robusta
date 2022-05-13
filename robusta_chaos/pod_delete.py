0a952477608b4b73cd8c193080cfd7f8ddebff9b
Your Name <you@example.com>
2022-05-13 11:34:41 +0400

    new

--- robusta_chaos/pod_delete.py
+++ robusta_chaos/pod_delete.py
@@ -2,8 +2,8 @@ from robusta.api import *
 
 @action
 def delete_pod(event: PrometheusKubernetesAlert):
-if not event.get_pod():
-    logging.info("alert is not related to pod")
-    return
+    if not event.get_pod():
+        logging.info("alert is not related to pod")
+        return
 
 event.get_pod().delete()
